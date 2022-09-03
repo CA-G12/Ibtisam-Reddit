bd99f83035a3194f80f1da76a8e5a931d7de3477
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-04 00:46:16 +0300

    Using index file for clean code and clean export and require

---
+++ server/database/queries/user/index.js
@@ -0,0 +1,9 @@
+const addUserQuery = require('./addUserQuery');
+const logInQuery = require('./logInQuery');
+const getAllUserInfo = require('./getAllUserInfo');
+
+module.exports = {
+    addUserQuery,
+    logInQuery,
+    getAllUserInfo
+};
