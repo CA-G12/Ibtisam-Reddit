24f13d4f92cf543623057934fa1e604f247552f7
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-03 15:25:44 +0300

    Adding Front-end Validation for both login and sign up processes relates #12

---
+++ client/public/JS/authentication/script.auth.js
@@ -0,0 +1,71 @@
+// Sign up operation
+signUp.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (usernameSign.value && emailSign.value && passwordSign.value && confirmPassword.value) {
+    if (usernameSign.value.length === 1) {
+      alert('Please enter a valid username');
+    } else if (passwordSign.value !== confirmPassword.value){
+      alert('The passwords must be matching');
+    } else if (!regexEmail.test(emailSign.value)){
+      alert('Please enter a valid email');
+    }else{
+      const userInfo = {
+        username: usernameSign.value,
+        email: emailSign.value,
+        password: passwordSign.value,
+        confirmPassword: confirmPassword.value,
+      }
+      signupFun(userInfo)
+    }
+  } else {
+    alert('Please Fill all the information needed to Sign Up');
+  }
+});
+
+function signupFun(userInfo) {
+  console.log(userInfo);
+  fetch(
+    '/users/signup',
+    Headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    method = 'POST',
+    body = JSON.stringify(userInfo),
+  )
+    .then((response) => {console.log(response); })
+    .catch((error) => { console.log(error); });
+}
+
+// Login operation
+login.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (email.value && password.value) {
+    if (!regexEmail.test(email.value)){
+      alert('Please enter a valid email');
+    }else{
+      const userInfo = {
+        email: email.value,
+        password: password.value,
+      };    
+      loginFun(userInfo)
+    }
+  } else {
+    alert('Please Fill all the information needed to Log in');
+  }
+});
+
+function loginFun(userInfo) {
+  console.log(userInfo);
+  fetch(
+    '/users/login',
+    Headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    method = 'POST',
+    body = JSON.stringify(userInfo),
+  )
+    .then((response) => {console.log(response); })
+    .catch((error) => { console.log(error); });
+}
