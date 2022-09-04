e4d3f7acf03a842fd363530edb9c157a6349929d
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-04 13:23:28 +0300

    Adding some queries needed for login process

--- server/database/queries/user/getAllUserInfo.js
+++ server/database/queries/user/getAllUserInfo.js
@@ -2,7 +2,7 @@ const connection = require('../../config/connection');
 
 const getAllUserInfo = (email) => {
     const sql = {
-        'text': 'SELECT email FROM users where email = $1',
+        'text': 'SELECT * from users where email = $1',
         'values': [email]
     }
     return connection.query(sql);
