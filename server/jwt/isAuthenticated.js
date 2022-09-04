4b59ca2694f5114cae3d760c638773008e6c3878
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-04 21:05:25 +0300

    Verify User whether he is logged in or not

---
+++ server/jwt/isAuthenticated.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+const isAuthenticated = (req, res, next) => {
+    const recievedToken = req.cookies.token;
+    // console.log(process.env.SECRET_KEY);
+    if(!recievedToken){
+        res.status(200).json({msg: 'Access denied'});
+    } else {
+        jwt.verify(recievedToken, process.env.SECRET_KEY, (err, decoded) => {
+            if (err) {
+            res.status(400).json({msg: 'Token invalid'});
+            } else {
+            res.cookie('username', decoded.username);
+            res.cookie('userId:', decoded.id);
+            next();
+            }
+        });
+    }
+}
+
+module.exports = isAuthenticated;
