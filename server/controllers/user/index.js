a13c7121ad81e981bc5b08af27eb2903b6a28a3c
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-04 22:37:51 +0300

    Handling log out process in the Back-end site

--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -1,9 +1,11 @@
 const signUp = require('./signUp');
 const login = require('./login');
 const homePage = require('./homePage');
+const logout = require('./logout');
 
 module.exports = {
     signUp,
     login,
-    homePage
+    homePage,
+    logout
 }
