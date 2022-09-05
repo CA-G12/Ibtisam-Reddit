0b2c59fc3040755eb709c98962da570d8b6c3957
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-05 14:11:53 +0300

    Adding the queries and route needed for rendering Posts in the home Page

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const cookieParser = require("cookie-parser");
 
 const { getPosts } = require('../controllers/posts/index');
-const isAuthenticated = require('../jwt/index');
+// const isAuthenticated = require('../jwt/index');
 
 router.use(cookieParser());
 
-router.get('/posts', getPosts);
+router.get('/post', getPosts);
 // router.post('/post/addpost', isAuthenticated, addPost);
 
 module.exports = router;
