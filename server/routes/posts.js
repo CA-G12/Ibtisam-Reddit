dbefdb7ec27b310d52af99510e29839c23259b7d
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-05 16:13:15 +0300

    Handling Get All Posts when Logged in in Server Side

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,6 +7,7 @@ const { getPosts } = require('../controllers/posts/index');
 router.use(cookieParser());
 
 router.get('/post', getPosts);
+router.get('homePost', getPosts);
 // router.post('/post/addpost', isAuthenticated, addPost);
 
 module.exports = router;
