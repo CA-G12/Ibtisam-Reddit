b96e9a7dafa6c776648f6952c7cebfd010207620
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-07 18:02:08 +0300

    Handling Error the right way and Removing all empty posts

---
+++ client/public/JS/dom.js
@@ -0,0 +1,8 @@
+const detailsContainer = document.querySelectorAll('.details-container');
+const showDetails = document.getElementById('show-details');
+
+detailsContainer.forEach((container)=>{
+    container.addEventListener('click',() => {
+        showDetails.classList.toggle('show-container');
+    })
+})
