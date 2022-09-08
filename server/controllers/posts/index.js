3c009029987ec6f30226a02322fd835dcd21cac3
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-09 02:00:49 +0300

    Start Preparing Edit Post

--- server/controllers/posts/index.js
+++ server/controllers/posts/index.js
@@ -2,10 +2,13 @@ const getPosts = require('./getPosts');
 const addPost = require('./addPost');
 const deletePost = require('./deletePost');
 const getComments = require('./getComments');
+const {editPost, getPost} = require('./editPost');
 
 module.exports = {
     getPosts,
     addPost,
     deletePost,
+    editPost,
+    getPost,
     getComments
 };
--- server/database/queries/posts/index.js
+++ server/database/queries/posts/index.js
@@ -1,11 +1,15 @@
 const getAllPostQuery = require('./getAllPosts');
+const getPostQuery = require('./getPostQuery');
 const addPostQuery = require('./addPostQuery');
 const deletePostQuery = require('./deletePostQuery');
+const editPostQuery = require('./editPostQuery');
 const getCommentsQuery = require('./getCommentsQuery');
 
 module.exports = {
     getAllPostQuery,
+    getPostQuery,
     addPostQuery,
     deletePostQuery,
+    editPostQuery,
     getCommentsQuery
 }
